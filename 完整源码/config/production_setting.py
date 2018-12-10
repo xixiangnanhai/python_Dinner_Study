0f52c42da583b9b7fdbd1b067f71e8666ee2b2ae
编_程_浪_子 <apanly@163.com>
2018-12-10 11:36:29 +0800

    更新 '完整源码/config/production_setting.py'

--- 完整源码/config/production_setting.py
+++ 完整源码/config/production_setting.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 DEBUG = True
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:58plPi56SdYP8a63@127.0.0.1/food_db?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql://username:password@127.0.0.1/food_db?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENCODING = "utf8mb4"
 
